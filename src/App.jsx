e27e7a4ff5919bd5663c65cfd2a83e6265f86cfa
Connor Roelofsen <connnorroelofsen15@gmail.com>
2021-09-15 16:10:16 +0200

    Create basic project scaffold.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="flexbox-content-wrapper">
+        
+      </div>
+    </>
+  )
+}
+
+export default App;
